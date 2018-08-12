a6b7577e28f9b599b48377ea37ae43b7cb1ba451
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2018-08-12 08:48:25 -0300

    Make some changes in view / Add edit and destroy method in TweetsController

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,28 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: %i(edit update destroy)
   def create
     @tweet = current_user.tweets.new(tweet_params)
 
     if @tweet.save
-      redirect_to root_url, notice: 'Post successfully created'
+      redirect_to root_url, notice: 'Tweet successfully created.'
     else
-      redirect_to root_url, alert: 'Error on creating new post'
+      redirect_to root_url, alert: 'Error on creating new post.'
     end
   end
 
+  def edit
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to root_path, notice: 'Tweeet was successfully destroyed.'
+  end  
+
   private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end 
 
   def tweet_params
     params.require(:tweet).permit(:content)
